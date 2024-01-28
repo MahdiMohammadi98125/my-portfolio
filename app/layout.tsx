c56450ba65b02dae086705802eb1e8c7edef02d6
MahdiMohammadi98125 <mahdi.mohammadi012021@gmail.com>
2024-01-28 21:52:51 +0430

    add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import ActiveSectionContextProvider from "@/context/ActiveSectionContext";
 import { Toaster } from "react-hot-toast";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
         <ActiveSectionContextProvider>
           <Header />
           {children}
+          <Footer />
           <Toaster position="top-right" />
         </ActiveSectionContextProvider>
       </body>
---
+++ components/Footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="px-4 mb-10 text-center text-gray-500">
+      <small className="block mb-2 text-xs">
+        &copy; {new Date().getFullYear()} Mahdi all right reserved.
+      </small>
+      <p className="text-xs ">
+        <span className="font-semibold">About this website:</span> built with
+        React & Next.js (App router & server Actions), Typescript, Tailwind css,
+        Framer Motion, React Email & Resend, Vercel hosting.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
